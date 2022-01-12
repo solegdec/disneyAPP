86ad123f743c691f5e9d5519b7b1329624c5e5b6
solegdec <soledecono@hotmail>
2022-01-12 11:17:38 -0300

    img

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports={
   "development": {
     "username": "root",
     "password": "",
-    "database": "alkemy_db",
+    "database": "disneyapp_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
